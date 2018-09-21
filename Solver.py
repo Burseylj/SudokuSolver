e25c133de5c2445b4d790be0e9c38e2207333df4
burseylj <burseylj@users.noreply.github.com>
2018-09-21 00:14:16 -0400

    finished solver

--- Solver.py
+++ Solver.py
@@ -19,9 +19,13 @@ def isSolved(state):
     for row in state:
         for cell in state:
             if cell not in nums:
-                return false
-    return true
+                return False
+    return True
 
+#function to move foreward with propagation algorithm
+# we update each cell's possible values by checking the taken values of it's
+# row, column and cell. If it only has one possible value we update it's value.
+# Return: foundDeadSpot, madeMove
 def propagateStep(state):
     madeMove = False
 
@@ -32,10 +36,10 @@ def propagateStep(state):
             if isinstance(state[row][col],set):
                 state[row][col] -= values
                 if (len(state[row][col]) == 1):
-                    state[i][col] = state[row][col].pop()
+                    state[row][col] = state[row][col].pop()
                     madeMove = True
                 elif (len(state[row][col]) == 0):
-                    return False, None
+                    return True, None
     #update columns
     for col in xrange(9):
         column = IsValid.getCol(state, col)
@@ -47,7 +51,7 @@ def propagateStep(state):
                     state[row][col] = state[row][col].pop()
                     madeMove = True
                 elif (len(state[row][col]) == 0):
-                    return False, None
+                    return True, None
     #update sub boxes
     for sub in xrange(9):
         subBox = IsValid.getSubBox(state,sub)
@@ -62,9 +66,42 @@ def propagateStep(state):
                         state[i][j] = state[i][j].pop()
                         madeMove = True
                     elif (len(state[i][j]) == 0):
-                        return False, None
+                        return True, None
                     
-    return True, madeMove
+    return False, madeMove
+
+# we move foreward with the propagation algorithm until we stop making progress
+def propagate(state):
+    while True:
+        foundDeadSpot, madeMove = propagateStep(state)
+        if foundDeadSpot:
+            return False
+        if not madeMove:
+            return True
+
+def solve(state):
+    solvable = propagate(state)
+    
+    if not solvable:
+        return None
+
+    if isSolved(state):
+        return state
+    
+    #if we can't make any certain moves, we make a guess.
+    #Build a new board with the guessed value and see if it works
+    for row in xrange(9):
+        for col in xrange(9):
+            cell = state[row][col]
+            if isinstance(cell, set):
+                for value in cell:
+                    newState = deepcopy(state)
+                    newState[row][cell] = value
+                    solved = solve(new_state)
+                    if solved is not None:
+                        return solved
+                return None
+
 
 emptyBoard = [['.']*9]*9
 badBoard = [nums*9]*9
@@ -87,8 +124,11 @@ assert [x for x in IsValid.getCol(getState(board1),0) if x in nums] == ['5', '6'
 assert [x for x in IsValid.getSubBox(getState(board1),4) if x in nums] == ['6', '8', '3', '2']
 assert state1[4][4] == '5'
 assert state1[6][5] == '7'
-assert propagateStep(getState(board1))[0] == True
-assert propagateStep(getState(badBoard))[0] == False
+assert propagateStep(getState(board1))[0] == False
+assert propagateStep(getState(badBoard))[0] == True
 assert propagateStep(getState(board1))[1] == True
 assert propagateStep(getState(emptyBoard))[1] == False
+solve(state1)
+assert IsValid.isValidSudoku(state1)
+print IO.makeBoard(state1)
 print "passed all tests"
